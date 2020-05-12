c6e0ef1d28d1e5e4452b91ed40b8b18689a63113
SenczykK <senczyk.kamil@gmail.com>
2020-05-12 20:54:00 +0200

    First

---
+++ src/main/java/senczykk/vpong/App.java
@@ -0,0 +1,13 @@
+package senczykk.vpong;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
